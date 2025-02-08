1824aece280cb5362784aafff99cbece411d105b
Cevdet Tufan <cevdet.tufan@hotmail.com>
2025-02-08 15:47:03 +0300

    #2 - migrations

---
+++ ConcertCleanArchitecture.Infrastructure/Migrations/20250208124238_initial.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcertCleanArchitecture.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Concerts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Venue = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Concerts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ConcertId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Row = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "money", nullable: false),
+                    IsReserved = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    ReservationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seats_Concerts_ConcertId",
+                        column: x => x.ConcertId,
+                        principalTable: "Concerts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Concert_Name",
+                table: "Concerts",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_ConcertId",
+                table: "Seats",
+                column: "ConcertId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Seats");
+
+            migrationBuilder.DropTable(
+                name: "Concerts");
+        }
+    }
+}
--- ConcertCleanArchitecture.Infrastructure/Persistence/AppDbContext.cs
+++ ConcertCleanArchitecture.Infrastructure/Persistence/AppDbContext.cs
@@ -6,4 +6,70 @@ internal class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(
 {
 	public DbSet<Concert> Concerts { get; set; } 
 	public DbSet<Seat> Seats { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Concert>(entity =>
+		{
+			entity.HasKey(e => e.Id);
+
+			entity.Property(e => e.Name)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			entity.HasIndex(e => e.Name)
+				.HasDatabaseName("IX_Concert_Name");
+
+			entity.Property(e => e.Date)
+				.IsRequired();
+
+			entity.Property(e => e.Venue)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			entity.Property(e => e.CreatedAt)
+				.HasDefaultValueSql("GETDATE()");
+
+			entity.Property(e => e.IsDeleted)
+				.HasDefaultValue(false);
+
+			entity.HasQueryFilter(e => !e.IsDeleted);
+
+			entity.HasMany(e => e.Seats);
+		});
+
+		modelBuilder.Entity<Seat>(entity =>
+		{
+			entity.HasKey(e => e.Id);
+
+			entity.Property(e => e.ConcertId)
+				.IsRequired();
+
+			entity.Property(e => e.Row)
+				.IsRequired()
+				.HasMaxLength(5);
+
+			entity.Property(e => e.Number)
+				.IsRequired();
+
+			entity.Property(e => e.Price)
+				.IsRequired()
+				.HasColumnType("money");
+
+			entity.Property(e => e.IsReserved)
+				.HasDefaultValue(false);
+
+			entity.Property(e => e.CreatedAt)
+				.HasDefaultValueSql("GETDATE()");
+
+			entity.Property(e => e.IsDeleted)
+				.HasDefaultValue(false);
+
+			entity.HasOne(e => e.Concert)
+				.WithMany(e => e.Seats)
+				.HasForeignKey(e => e.ConcertId);
+
+			entity.HasQueryFilter(e => !e.IsDeleted);
+		});
+	}
 }
