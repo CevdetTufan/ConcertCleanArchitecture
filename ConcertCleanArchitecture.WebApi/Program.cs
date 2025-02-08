e3c037841c3b0aca516488d3076c7539e277d9c0
Cevdet Tufan <cevdet.tufan@hotmail.com>
2025-02-08 15:21:30 +0300

    #1 - added references to api.

--- ConcertCleanArchitecture.WebApi/Program.cs
+++ ConcertCleanArchitecture.WebApi/Program.cs
@@ -1,8 +1,14 @@
 using Scalar.AspNetCore;
+using ConcertCleanArchitecture.Infrastructure;
+using ConcertCleanArchitecture.Application;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services
+	.AddInfrastructure(builder.Configuration)
+	.AddApplication();
 
 var app = builder.Build();
 
